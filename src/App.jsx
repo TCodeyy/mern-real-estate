fac4ceca831181ea0451272da764ac2cd8c41730
Tanya Tyagi <tyagitanya2102@gmail.com>
2025-04-26 15:16:09 -0400

    created header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import SignIn from './pages/SignIn';
 import Home from './pages/Home';
 import About from './pages/about';
 import Profile from './pages/Profile'
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header/>
    <Routes>
     <Route path="/" element={<Home/>}/>
     <Route path="/sign-in" element={<SignIn/>}/>
