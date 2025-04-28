c7a9e36d10a1acff0dd20b5a0d184ed1e31a35ec
Tanya Tyagi <tyagitanya2102@gmail.com>
2025-04-27 20:54:48 -0400

    created user update api

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,13 +2,14 @@
 
 
 import express from 'express';
-import { test } from '../controllers/user.controller.js';
+import { test, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 
 const router = express.Router()
 
-
 router.get('/test', test)
+router.post('/update/:id', verifyToken , updateUser)
 
 
 // we will put the logics in controllers folder and not here
--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
+import {useRef} from 'react'
 
 export default function Profile() {
+
+  const fileRef = useRef(null)
   const {currentUser} = useSelector((state) => state.user)
+const [file, setFile] = useState(undefined)
+
+console.log('file', file)
+
+useEffect(() => {
+  if(file){
+    handleFileUpload(file)
+  }
+}, [file])
+
+const handleFileUpload =(file) => {
+ // will handle later 
+
+  
+}
   return (
     <div className='p-3 max-w-lg mx-auto'>
    <h1 className='text-3xl font-semibold text-center my-7'>Profile</h1>
    <form className='flex flex-col gap-4'>
-    <img className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src= {currentUser.avatar} alt='profile'></img>
+    <input onChange={(e) => setFile(e.target.files[0])}type='file' ref={fileRef} hidden accept='image/*'></input>
+    <img onClick={() => fileRef.current.click()}className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src= {currentUser.avatar} alt='profile'></img>
    <input id='username' type='text' placeholder='username' className='border p-3 rounded-lg'></input>
    <input id='email' type='email' placeholder='email' className='border p-3 rounded-lg'></input>
    <input id='password' type='text' placeholder='password' className='border p-3 rounded-lg'></input>
