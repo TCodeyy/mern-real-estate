121aad966f60c0d8cf0865d80f7b7c8a6ba230e2
Tanya Tyagi <tyagitanya2102@gmail.com>
2025-04-29 21:48:53 -0400

    getListngs api completed

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,7 @@
 
 
 import express from 'express';
-import { test, updateUser , deleteUser} from '../controllers/user.controller.js';
+import { test, updateUser , deleteUser, getUserListings} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
 
@@ -11,6 +11,7 @@ const router = express.Router()
 router.get('/test', test)
 router.post('/update/:id', verifyToken , updateUser)
 router.delete('/delete/:id', verifyToken , deleteUser)
+router.get('/listings/:id', verifyToken, getUserListings)
 
 
 // we will put the logics in controllers folder and not here
