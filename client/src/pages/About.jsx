c3e76b507eef7ab1c60587d904fafe815833af5e
Tanya Tyagi <tyagitanya2102@gmail.com>
2025-05-04 12:16:42 -0400

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4"> About Mern-Real-Estate</h1>
+      <p className="mb-4 text-slate-700">
+        This real estate web application was built as part of a guided MERN
+        stack project from Udemy, but it represents much more than just
+        following a tutorial. After taking a year-long break from tech, I used
+        this project as a way to reconnect with the tools, concepts, and
+        confidence that originally sparked my interest in development.
+      </p>
+      <p className="mb-4 text-slate-700">
+        To make it truly my own, I went beyond the course by adding a web
+        recognition feature—an enhancement I implemented independently. It was
+        both a technical challenge and a reminder of how far I've come. Through
+        this process, I revisited core technologies like React, Node.js,
+        MongoDB, and Express, and gave myself a hands-on refresher that quickly
+        brought everything back under my control.
+      </p>
+      <p className="mb-4 text-slate-700">
+        This project wasn’t just about building a real estate platform—it was
+        about rebuilding momentum, reigniting curiosity, and proving to myself
+        that I’m ready to create again.
+      </p>
     </div>
-  )
+  );
 }
