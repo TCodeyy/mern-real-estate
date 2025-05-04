c454ef349406eab27c9ce62d3051cc1c445ce953
Tanya Tyagi <tyagitanya2102@gmail.com>
2025-05-04 12:56:18 -0400

    Fix typo for About page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
-import About from './pages/about';
+import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
